43f02eb057a0dc77d9989f846afad4b67f3b84a1
Law Wai Chun <chunlaw@rocketmail.com>
2023-11-13 23:38:42 +0800

    Workable demo

--- src/components/Compass.tsx
+++ src/components/Compass.tsx
@@ -1,5 +1,30 @@
-import { Box, Typography } from "@mui/material";
-import useCompass from "react-world-compass";
+import { Box, Button, Typography } from "@mui/material";
+import { useState } from "react";
+import useCompass, { requestPermission, isSafari } from "react-world-compass";
+
+const CompassWrapper = () => {
+  const [permission, setPermission] = useState("default")
+
+  const handleClick = () => {
+    requestPermission()
+      .then(_permission => {
+        setPermission(_permission)
+      })
+  }
+
+  if ( isSafari && permission !== "granted" ) {
+    return (
+      <Box flex="1" display="flex" alignItems="center" justifyContent="center">
+        <Button variant="contained" onClick={handleClick}>
+          Request Device Permission
+        </Button>
+      </Box>
+    )
+  }
+  return (
+    <Compass />
+  )
+}
 
 const Compass = () => {
   const deg = useCompass(100);
@@ -27,4 +52,4 @@ const Compass = () => {
   );
 };
 
-export default Compass;
+export default CompassWrapper;
--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,9 +1,18 @@
 import { GitHub as GitHubIcon } from "@mui/icons-material";
 import { Box, IconButton, SxProps, Theme } from "@mui/material";
+import npmLogo from "../../assets/npm.svg";
 
 const Footer = () => {
   return (
     <Box sx={rootSx}>
+      <IconButton
+        onClick={() => {
+          window.open("https://www.npmjs.com/package/react-world-compass", "_blank");
+        }}
+        size="small"
+      >
+        <img src={npmLogo} width={24} height={24} alt="NPM logo" />
+      </IconButton>
       <IconButton
         onClick={() => {
           window.open(
